3214aa75b9c4e8fd1e5b6a124815828ce056fc69
HelloBetafish <bethany.pfeister@gmail.com>
2017-10-20 12:01:58 -0700

    db, config, models, and server.js set-up done

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Bleu Cheese and Black Pepper');
+INSERT INTO burgers (burger_name) VALUES ('Goat Cheese and Serrano Pepper');
+INSERT INTO burgers (burger_name) VALUES ('Classic Cheeseburger');
